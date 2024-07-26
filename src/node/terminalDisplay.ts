5fe7401dd7a421b03ecfa73dd15ca27bbe06efac
Jacob Knowlton <knowlton.jacob17@gmail.com>
2024-07-26 15:35:38 -0600

    Added terminal keyboard and display functionality

--- src/node/terminalDisplay.ts
+++ src/node/terminalDisplay.ts
@@ -16,12 +16,8 @@ export class TerminalDisplay extends Display {
 
     this.terminal = terminal;
     this.terminal.fullscreen(true);
+    this.terminal.hideCursor();
 
-    // this.terminal.grabInput(true);
-    // this.terminal.on("key", (name: any, matches: any, data: any) => {
-    //   console.log(name, matches, data);
-    //   if (name === "q") process.exit();
-    // })
     // this.terminal.inputField((_, name) => {
     //   this.terminal.green("\nHello!\n", name, "\n");
     //   process.exit();
@@ -57,32 +53,49 @@ export class TerminalDisplay extends Display {
     if (this.isLayerChanged.every(item => item === false))
       return;
 
-    let screenStr = "";
+    let screenStr: Array<string> = [];
+
     if (this.screenWidth === TerminalDisplay.LOW_RES_WIDTH && this.screenHeight === TerminalDisplay.LOW_RES_HEIGHT) {
+      screenStr = new Array<string>(this.screenHeight);
       for (let i = 0; i < TerminalDisplay.LOW_RES_HEIGHT; i++) {
+        let line = "";
         for (let j = 0; j < TerminalDisplay.LOW_RES_WIDTH; j++) {
           const idx = i * TerminalDisplay.LOW_RES_WIDTH + j;
-          screenStr += this.lowResChars[this.screen[0][idx] ? 1 : 0]
+          line += this.lowResChars[this.screen[0][idx] ? 1 : 0]
         }
-        screenStr += '\n'
+        screenStr[i] = line;
       }
     }
     else if (this.screenWidth === TerminalDisplay.HIGH_RES_WIDTH && this.screenHeight === TerminalDisplay.HIGH_RES_HEIGHT) {
+      screenStr = new Array<string>(this.screenHeight / 2);
       for (let i = 0; i < TerminalDisplay.HIGH_RES_HEIGHT; i += 2) {
+        let line = "";
         for (let j = 0; j < TerminalDisplay.HIGH_RES_WIDTH; j += 2) {
           const idx1 = i * TerminalDisplay.HIGH_RES_WIDTH + j;
           const idx2 = idx1 + 1;
           const idx3 = idx1 + TerminalDisplay.HIGH_RES_WIDTH;
           const idx4 = idx3 + 1;
           const block =
-            (this.screen[0][idx1] ? 1 : 0) |
-            (this.screen[0][idx2] ? 2 : 0) |
-            (this.screen[0][idx3] ? 4 : 0) |
-            (this.screen[0][idx4] ? 8 : 0);
-          screenStr += this.highResChars[block];
+            ((this.screen[0][idx1] || this.screen[1][idx1]) ? 1 : 0) |
+            ((this.screen[0][idx2] || this.screen[1][idx2]) ? 2 : 0) |
+            ((this.screen[0][idx3] || this.screen[1][idx3]) ? 4 : 0) |
+            ((this.screen[0][idx4] || this.screen[1][idx4]) ? 8 : 0);
+          line += this.highResChars[block];
         }
-        screenStr += '\n';
+        screenStr[i / 2] = line;
       }
     }
+
+    for (let lineIdx = 0; lineIdx < screenStr.length; lineIdx++) {
+      this.screenBuffer.put({
+        x: 0,
+        y: lineIdx,
+        attr: 255,
+        wrap: false,
+        dx: 1,
+        dy: 0
+      }, screenStr[lineIdx])
+    }
+    this.screenBuffer.draw();
   }
 }
--- src/node/terminalKeyboard.ts
+++ src/node/terminalKeyboard.ts
@@ -8,5 +8,27 @@ export class TerminalKeyboard extends Keyboard {
     super();
 
     this.terminal = terminal;
+    this.terminal.grabInput(true);
+    this.terminal.on("key", (name: string) => {
+      if (name === "CTRL_C" || name === "ESCAPE") {
+        this.terminal.grabInput(false);
+        this.terminal.hideCursor(false);
+        this.terminal.processExit(0);
+      }
+      if (name in this.keyMap) {
+        this.pressedKeys[this.keyMap[name]] = true;
+        if (this.waiting) {
+          this.waitingKey = this.keyMap[name];
+          this.waiting = false;
+          this.pressedKeys[this.keyMap[name]] = false;
+        }
+      }
+
+    })
+
+    setInterval(() => {
+      for (let key in this.pressedKeys)
+        this.pressedKeys[key] = false;
+    }, 50);
   }
 }
